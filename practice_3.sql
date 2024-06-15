912298d05ceff3b35a9b7097d2d183e001dda8ba
tmhaan <han.trinh3094@gmail.com>
2024-06-15 21:55:48 +0700

    Update practice_3.sql

--- practice_3.sql
+++ practice_3.sql
@@ -3,8 +3,20 @@ select Name from STUDENTS
     where Marks > 75
     order by (right(Name,3)), ID;
 
---Ex2:leetcode-fix-names-in-a-table.
+--Ex2: leetcode-fix-names-in-a-table.
 select user_id,
     concat(upper(left(name, 1)), lower(substring(name,2,length(name)))) as name
     from Users
 order by user_id;
+
+--Ex3: datalemur-total-drugs-sales
+select manufacturer,
+  '$' || left(sale, -6) || ' million' as sale
+from (
+  select manufacturer, sum(total_sales),
+   round(sum(total_sales), -6)::varchar AS sale
+  from pharmacy_sales 
+  group by manufacturer
+  order by sum(total_sales) desc
+)A
+
