42e2ac1506859d30eb826878354203f2cfd21781
tmhaan <han.trinh3094@gmail.com>
2024-06-16 22:06:52 +0700

    Update practice_4.sql

--- practice_4.sql
+++ practice_4.sql
@@ -15,16 +15,23 @@ SELECT
 FROM Triangle;
 
 --Ex3: datalemur-uncategorized-calls-percentage.
-select *,
-  round(((count_null*100)/total),1) as uncategorised_call_pct
+SELECT 
+  round(((count_null*100)/ total),1) as uncategorised_call_pct
 from (
-  select
-  count(*) as total,
-  count(case
-    when call_category = 'n/a' or call_category = 'NULL' then 1
-  end) as count_null
-  FROM callers
-)A 
+  select 
+  sum((case when call_category IS NULL or call_category = 'n/a' then 1 end)) as count_null,
+  count(*)::numeric as total
+  from callers
+)A
 
 --Ex4: datalemur-find-customer-referee.
+select name from Customer
+    where not referee_id = 2 or referee_id is null;
+
 --Ex5: stratascratch the-number-of-survivors.
+select survived,
+    count(case when pclass = 1 then 1 end) as first_class,
+    count(case when pclass = 2 then 1 end) as second_class,
+    count(case when pclass = 3 then 1 end) as third_class
+from titanic
+group by survived;
