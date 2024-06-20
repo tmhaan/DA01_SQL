981e76f14bf10a95ba055be666e70f27ca597287
tmhaan <han.trinh3094@gmail.com>
2024-06-20 14:02:44 +0700

    Update practice_7.sql

--- practice_7.sql
+++ practice_7.sql
@@ -19,9 +19,51 @@ group by yr, a.product_id, curr_year_spend
 order by a.product_id, a.yr)x
 
 --ex2: datalemur-card-launch-success.
+SELECT  * from (
+  select distinct card_name,
+  first_value (issued_amount) over (partition by card_name order by issue_year, issue_month) as issued_amount
+  FROM monthly_cards_issued
+  )A
+order by issued_amount desc
+
 --ex3: datalemur-third-transaction.
+select user_id, spend, transaction_date from 
+  (select user_id, spend, transaction_date,
+  row_number() over (partition by user_id order by transaction_date) as row_num
+  from transactions
+  order by user_id, transaction_date)A
+where row_num = 3
+
 --ex4: datalemur-histogram-users-purchases.
+Select transaction_date, user_id, purchase_count from (
+  SELECT transaction_date, user_id, count(product_id) purchase_count
+  FROM user_transactions
+  group by user_id, transaction_date
+  order by transaction_date DESC
+  limit 3)A
+order by transaction_date
+
 --ex5: datalemur-rolling-average-tweets.
+with t1 as (
+    SELECT user_id , tweet_date , tweet_count
+    from tweets
+    order by 1 , 2 desc )
+
+select user_id , tweet_date ,
+(select round(avg(tweet_count),2) from tweets 
+where user_id = t1.user_id 
+  and tweet_date between t1.tweet_date - INTERVAL '2 DAY' and t1.tweet_date )
+from t1
+order by 1 , 2
+
 --ex6: datalemur-repeated-payments.
+WITH a as(
+SELECT *,
+Extract(
+minute FROM transaction_timestamp - lag(transaction_timestamp) OVER(PARTITION BY merchant_id,credit_card_id,amount)) as flag 
+from transactions)
+select count(*) as payment_count from a
+where flag < 10
+
 --ex7: datalemur-highest-grossing.
 --ex8: datalemur-top-fans-rank.
