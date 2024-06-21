3a2d32284fc73ab6ddbec3f858702ba5aadcbd13
tmhaan <han.trinh3094@gmail.com>
2024-06-21 15:09:40 +0700

    Update practice_8.sql

--- practice_8.sql
+++ practice_8.sql
@@ -10,10 +10,40 @@ select round(y,2) as immediate_percentage from (select
     where row_num = 1)A
 
 --ex2: leetcode-game-play-analysis.
+SELECT
+  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+  IN (SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id)
 
 --ex3: leetcode-exchange-seats.
+with a as 
+(select student, id - 1 as new_id from Seat 
+where id%2 = 0
+union
+select student, 
+case when odd_id = 1 then id else id + 1 end as new_id from (
+    Select student, id, 
+    row_number() over (order by id desc) as odd_id 
+    from Seat 
+    where id % 2 = 1)A)
+
+select new_id as id, student 
+    from a 
+order by id
 
 --ex4: leetcode-restaurant-growth.
+With a as 
+(select visited_on, sum(amount) as total_amount from Customer 
+group by visited_on)
+
+SELECT * from (select 
+        visited_on,
+         SUM(total_amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+         ROUND(AVG(total_amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
+    FROM a)A
+    where visited_on >= min(visited_on) + 6
+    order by visited_on
 
 --ex5: leetcode-investments-in-2016.
 
