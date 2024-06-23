cc1b86509000a3a8d19738dc77c66966d1dc2a1a
tmhaan <han.trinh3094@gmail.com>
2024-06-23 16:36:21 +0700

    Update project_1.sql

--- project_1.sql
+++ project_1.sql
@@ -1,1 +1,8 @@
-
+alter table sales_dataset_rfm_prj
+alter column ordernumber type integer using ordernumber::numeric,
+alter column quantityordered type integer using quantityordered::numeric,
+alter column priceeach type decimal using priceeach::numeric,
+alter column orderlinenumber type integer using orderlinenumber::numeric,
+alter column sales type decimal using sales::numeric,
+alter column msrp type integer using msrp::numeric, 
+alter column orderdate type date using to_date(orderdate, 'MM/DD/YYYY')
